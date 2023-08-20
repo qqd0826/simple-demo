d140bb301e85e9419803d6e5c8ec51ec9a61a30e
one-fishcat <90766418+one-fishcat@users.noreply.github.com>
2023-08-20 09:55:03 +0800

    Add files via upload

---
+++ simple-demo-main/controller/user.go
@@ -0,0 +1,168 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func init() {
+	username := "root"
+	password := "salt"
+	host := "127.0.0.1"
+	port := 3306
+	Dbname := "test"
+	timeout := "3s"
+
+	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//fmt.Println(db)
+
+	db.AutoMigrate(&User{})
+	DB = db
+}
+
+// usersLoginInfo use map to store user info, and key is username+password for demo
+// user data will be cleared every time the server starts
+// // test data: username=zhanglei, password=douyin
+var usersLoginInfo = map[string]User{
+	"zhangleidouyin": {
+		Id:            1,
+		Name:          "zhanglei",
+		FollowCount:   10,
+		FollowerCount: 5,
+		IsFollow:      true,
+	},
+}
+
+var userIdSequence = int64(1)
+
+type UserLoginResponse struct {
+	Response
+	UserId int64  `json:"user_id,omitempty"`
+	Token  string `json:"token"`
+}
+
+type UserResponse struct {
+	Response
+	User User `json:"user"`
+}
+
+func Register(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	token := username + password
+	user := User{}
+	if ret := DB.Where("username = ?", username).First(&user); ret.Error == nil {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
+		})
+	} else {
+		atomic.AddInt64(&userIdSequence, 1)
+		newUser := User{
+			Id:       userIdSequence,
+			Name:     username,
+			Password: password,
+			Token:    token,
+		}
+		DB.Create(&newUser)
+		// fmt.Println("*****************")
+		//usersLoginInfo[token] = newUser
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 0},
+			UserId:   userIdSequence,
+			Token:    username + password,
+		})
+	}
+
+	//if _, exist := usersLoginInfo[token]; exist {
+	//	c.JSON(http.StatusOK, UserLoginResponse{
+	//		Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
+	//	})
+	//} else {
+	//	atomic.AddInt64(&userIdSequence, 1)
+	//	newUser := User{
+	//		Id:   userIdSequence,
+	//		Name: username,
+	//	}
+	//	usersLoginInfo[token] = newUser
+	//	c.JSON(http.StatusOK, UserLoginResponse{
+	//		Response: Response{StatusCode: 0},
+	//		UserId:   userIdSequence,
+	//		Token:    username + password,
+	//	})
+	//}
+}
+
+func Login(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	token := username + password
+
+	var user = User{}
+	if ret := DB.Where("username = ? and password = ?", username, password).First(&user); ret.Error != nil {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 0},
+			UserId:   user.Id,
+			Token:    token,
+		})
+	} else {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist or Password wrong"},
+		})
+	}
+
+	//if user, exist := usersLoginInfo[token]; exist {
+	//	c.JSON(http.StatusOK, UserLoginResponse{
+	//		Response: Response{StatusCode: 0},
+	//		UserId:   user.Id,
+	//		Token:    token,
+	//	})
+	//} else {
+	//	c.JSON(http.StatusOK, UserLoginResponse{
+	//		Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+	//	})
+	//}
+}
+
+func UserInfo(c *gin.Context) {
+	token := c.Query("token")
+
+	var user User
+	if exist := DB.Where("token = ?", token).First(&user); exist != nil {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 0},
+			User:     user,
+		})
+	} else {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{
+				StatusCode: 1, StatusMsg: "User doesn't exist"},
+		})
+	}
+
+	//if user, exist := usersLoginInfo[token]; exist {
+	//	c.JSON(http.StatusOK, UserResponse{
+	//		Response: Response{StatusCode: 0},
+	//		User:     user,
+	//	})
+	//} else {
+	//	c.JSON(http.StatusOK, UserResponse{
+	//		Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+	//	})
+	//}
+}
