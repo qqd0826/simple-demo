e2a7a06f7fac7e4f5c6f1f61e375e71b6a02a413
qiuqidong <2747347675@qq.com>
2023-08-24 14:20:44 +0800

    视频的封面

--- controller/publish.go
+++ controller/publish.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/RaymondCode/simple-demo/db"
 	"github.com/RaymondCode/simple-demo/model"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 )
@@ -34,6 +36,7 @@ func Publish(c *gin.Context) {
 
 	// 接收数据
 	data, err := c.FormFile("data")
+
 	if err != nil {
 		c.JSON(http.StatusOK, model.Response{
 			StatusCode: 1,
@@ -41,24 +44,45 @@ func Publish(c *gin.Context) {
 		})
 		return
 	}
-
-	// 保存数据
-	filename := filepath.Base(data.Filename)
-	finalName := fmt.Sprintf("%d_%s", user.Id, filename)
-	saveFile := filepath.Join("./public/", finalName)
-	if err := c.SaveUploadedFile(data, saveFile); err != nil {
-		c.JSON(http.StatusOK, model.Response{
-			StatusCode: 1,
-			StatusMsg:  err.Error(),
-		})
-		return
+	// 创建OSSClient实例。
+	// yourEndpoint填写Bucket对应的Endpoint，以北京为例，填写为https://oss-cn-beijing.aliyuncs.com。其它Region请按实际情况填写。
+	// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+	client, err := oss.New(
+		"https://oss-cn-beijing.aliyuncs.com",
+		"LTAI5tDPiHQY8DXVUv25mhQH",
+		"ej9I1AAHc6OM7DP4W70W9l18074lrH",
+	)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
 	}
+
+	// 填写存储空间名称，例如examplebucket。
+	bucket, err := client.Bucket("qqd-simple-demo")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	//保存到oss
+	file, _ := data.Open()
+	filename := filepath.Base(data.Filename)
+	finalName := fmt.Sprintf("%d-%v-%s", user.Id, time.Now().Unix(), filename)
+	err = bucket.PutObject(finalName, file)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	//获得oss存储中的url，以及视频抽帧功能获得第一毫秒的帧作为封面。
+	playurl := fmt.Sprintf("%s%s", "https://qqd-simple-demo.oss-cn-beijing.aliyuncs.com/", finalName)
+	coverurl := fmt.Sprintf("%s%s", playurl, "?x-oss-process=video/snapshot,t_0001,f_jpg,w_800,h_600,m_fast")
 
 	// 保存到数据库
 	newVideo := model.Video{
 		Author:     user,
-		PlayUrl:    saveFile,
-		CoverUrl:   saveFile,
+		PlayUrl:    playurl,
+		CoverUrl:   coverurl,
 		UpLoadTime: time.Now().Unix(),
 		Title:      title,
 	}
@@ -68,17 +92,40 @@ func Publish(c *gin.Context) {
 		StatusCode: 0,
 		StatusMsg:  finalName + " uploaded successfully",
 	})
+
+	//// 保存数据
+	//filename := filepath.Base(data.Filename)
+	//finalName := fmt.Sprintf("%d_%s", user.Id, filename)
+	//saveFile := filepath.Join("./public/", finalName)
+	//if err := c.SaveUploadedFile(data, saveFile); err != nil {
+	//	c.JSON(http.StatusOK, model.Response{
+	//		StatusCode: 1,
+	//		StatusMsg:  err.Error(),
+	//	})
+	//	return
+	//}
+	//// 保存到数据库
+	//newVideo := model.Video{
+	//	Author:     user,
+	//	PlayUrl:    saveFile,
+	//	CoverUrl:   saveFile,
+	//	UpLoadTime: time.Now().Unix(),
+	//	Title:      title,
+	//}
+	//db.DB.Create(&newVideo)
+	//
+	//c.JSON(http.StatusOK, model.Response{
+	//	StatusCode: 0,
+	//	StatusMsg:  finalName + " uploaded successfully",
+	//})
 }
 
 // PublishList all users have same publish video list
 func PublishList(c *gin.Context) {
-	token := c.Query("token")
-	user := model.User{}
-
-	if res := db.DB.Where("username = ?", token).First(&user); res.Error == nil {
-		c.JSON(http.StatusOK, VideoListResponse{
-			Response:  model.Response{StatusCode: 0},
-			VideoList: getVideo(),
-		})
-	}
+	c.JSON(http.StatusOK, VideoListResponse{
+		Response: model.Response{
+			StatusCode: 0,
+		},
+		VideoList: getVideo(),
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/RaymondCode/simple-demo
 go 1.17
 
 require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.8+incompatible
 	github.com/gavv/httpexpect/v2 v2.8.0
 	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
@@ -49,6 +50,7 @@ require (
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
