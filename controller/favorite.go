8ad16f560584351463e8856441c3e28517d9ad3a
Fantasy <jiandan3013@163.com>
2023-08-29 11:49:21 +0800

    增加点赞功能

--- controller/favorite.go
+++ controller/favorite.go
@@ -1,20 +1,47 @@
 package controller
 
 import (
+	"github.com/RaymondCode/simple-demo/db"
 	"github.com/RaymondCode/simple-demo/model"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
 	token := c.Query("token")
+	videoId := c.Query("video_id")
+	actionType := c.Query("action_type")
 
-	if _, exist := usersLoginInfo[token]; exist {
+	// 检验用户是否存在
+	user := model.User{}
+	if res := db.DB.Where("username = ?", token).First(&user); res.Error == nil {
 		c.JSON(http.StatusOK, model.Response{StatusCode: 0})
-	} else {
-		c.JSON(http.StatusOK, model.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+
+		var video model.Video
+		db.DB.Where("id = ?", videoId).First(&video)
+
+		if actionType == "1" {
+			db.DB.Model(&video).Update("favorite_count", gorm.Expr("favorite_count + ?", 1))
+		} else if actionType == "2" {
+			db.DB.Model(&video).Update("favorite_count", gorm.Expr("favorite_count - ?", 1))
+		}
+
+		if video.FavoriteCount > 0 {
+			db.DB.Model(&video).Update("is_favorite", true)
+		} else {
+			db.DB.Model(&video).Update("is_favorite", false)
+		}
+	} else { // 不存在
+		c.JSON(http.StatusOK, model.Response{StatusCode: 1, StatusMsg: "用户未登录，请先登录"})
 	}
+
+	//if _, exist := usersLoginInfo[token]; exist {
+	//	c.JSON(http.StatusOK, model.Response{StatusCode: 0})
+	//} else {
+	//	c.JSON(http.StatusOK, model.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	//}
 }
 
 // FavoriteList all users have same favorite video list
