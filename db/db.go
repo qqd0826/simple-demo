654b97c27e2e36fe0b7e11d3d783bfa05601adca
Fantasy <jiandan3013@163.com>
2023-08-29 18:19:03 +0800

    加了个mssql驱动

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/RaymondCode/simple-demo/model"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "gorm.io/driver/mysql"
 	"log"
 )
@@ -10,8 +11,14 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	connect := "root:123456@tcp(localhost:3306)/godb?charset=utf8&parseTime=True&loc=Local"
+	/*connect := "root:123456@tcp(localhost:3306)/godb?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", connect)
+	if err != nil {
+		log.Fatal(err)
+	}*/
+
+	connect := "server=127.0.0.1;port=1433;database=dy;user id=sa;password=963013"
+	db, err := gorm.Open("mssql", connect)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,6 +26,7 @@ func InitDB() {
 	DB = db
 	DB.AutoMigrate(&model.User{})
 	DB.AutoMigrate(&model.Video{})
+	DB.AutoMigrate(&model.FavoriteData{})
 	// 这里可以继续添加其他需要迁移的表
 	// 若不存在数据库表会自动生成
 
